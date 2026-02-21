737bdec21d46ee43e0ce86bf4e404733fb597ca9
St3v3-3676 <stephen_boyle48@outlook.com>
2026-02-21 08:42:46 +0000

    Initial Commit

---
+++ GCSEResultsAndGradeCalculatorApp/GCSEResultsAndGradeCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCSEResultsAndGradeCalculatorAppApp.swift
+//  GCSEResultsAndGradeCalculatorApp
+//
+//  Created by Stephen Boyle on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GCSEResultsAndGradeCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
